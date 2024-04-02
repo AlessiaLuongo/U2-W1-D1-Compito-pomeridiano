1d8d8f44d0645ce9c4ce605de24eec8e819d540f
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-04-02 14:12:28 +0200

    first commit

---
+++ src/test/java/alessia/U2W1D1/U2W1D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package alessia.U2W1D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
